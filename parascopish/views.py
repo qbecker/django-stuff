91bc1d1d1d9491bc8de20e114fc53089e617d1af
Quinten Becker <qbecker@asu.edu>
2017-08-28 20:54:24 +0000

    some changes

--- parascopish/views.py
+++ parascopish/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    
+    posts = Post.objects.all()
+    return render(request, 'parascopish/post_list.html', {'posts': posts})
+    
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    posts = Post.objects.all()
+    return render(request, 'parascopish/post_detail.html', {'post': post, 'posts': posts})
+    
